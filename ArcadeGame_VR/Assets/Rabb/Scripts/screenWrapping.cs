de8a1c9da336f551f8f0d6063eeab6d0d6f1fda4
Rabben82 <Christian.Rabb@yh.nackademin.se>
2022-11-15 13:22:40 +0100

    Final changes Master

--- ArcadeGame_VR/Assets/Rabb/Scripts/screenWrapping.cs
+++ ArcadeGame_VR/Assets/Rabb/Scripts/screenWrapping.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class screenWrapping : MonoBehaviour
 {
-    private float posZ = -0.05f;
-    // Use this for initialization
-
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.x > 7.3f)
         {
-            float newPosX = transform.position.x * -1 + 0.1f;
-            transform.position = new Vector3(newPosX, transform.position.y, posZ);
+            ChangePositionX(-1, +0.1f);
+            
         }
 
         if (transform.position.x < -7.3f)
         {
-            float newPosX = transform.position.x * -1 - 0.1f;
-            transform.position = new Vector3(newPosX, transform.position.y, posZ);
+            ChangePositionX(-1, -0.1f);          
         }
 
         if (transform.position.y > 5.52f)
         {
-            float newPosY = transform.position.y * -1 + 0.1f;
-            transform.position = new Vector3(transform.position.x, newPosY, posZ);
+            ChangePositionY(-1, +0.1f);
+           
         }
         if (transform.position.y < -5.52f)
         {
-            float newPosY = transform.position.y * -1 - 0.1f;
-            transform.position = new Vector3(transform.position.x, newPosY, posZ);
+            ChangePositionY(-1, -0.1f);
+          
         }
     }
-}
 
+    void ChangePositionX(int posXwrap, float jittersafeX)
+    {
+        float newPosX = transform.position.x * posXwrap + jittersafeX;
+        transform.position = new Vector3(newPosX, transform.position.y);
+    }
+
+    void ChangePositionY(int posYwrap, float jittersafeY)
+    {
+        float newPosY = transform.position.y * posYwrap + jittersafeY;
+        transform.position = new Vector3(transform.position.x, newPosY);
+    }
+}
